efde6ea6e507ff6c42819b88a11ebaf6f79b2398
Rogerio Nascimento <rogerio@rogerionascimento.com>
2020-05-19 11:17:40 +0100

    Layout setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    // $: 'jquery/src/jquery',
+    // jQuery: 'jquery/src/jquery',
+    $: 'jquery',
+    jQuery: 'jquery',
+    jq: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    'window.$': 'jquery',
+    'window.jQuery': 'jquery',
+    Waves: 'node-waves'
+  })
+)
+
+const aliasConfig = {
+  'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
+  'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+};
+
+environment.config.set('resolve.alias', aliasConfig);
 module.exports = environment
